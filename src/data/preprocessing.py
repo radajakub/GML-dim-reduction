81a6caa2d7764027fc72f9bffe55e71544267ec6
radajakub <dev.jakubrada@icloud.com>
2023-05-18 20:01:02 +0900

    add shuffling function

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -2,6 +2,14 @@ import numpy as np
 
 
 # TODO what if two same data points have different labels?
-def remove_duplicities(data, labels):
+def remove_duplicities(data, labels, shuffle=True):
     _, unique_indices = np.unique(data, return_index=True, axis=0)
-    return data[unique_indices, :], labels[unique_indices]
+    data, labels = data[unique_indices, :], labels[unique_indices]
+    if shuffle:
+        data, labels = shuffle_data(data, labels)
+    return data, labels
+
+
+def shuffle_data(data, labels):
+    random_idx = np.random.permutation(labels.shape[0])
+    return data[random_idx, :], labels[random_idx]
