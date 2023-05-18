eaf20d9568d778dbb0cd02c01cb36ac7508dd5eb
radajakub <dev.jakubrada@icloud.com>
2023-05-18 13:37:45 +0900

    Add customizable weights for graph construction

--- src/utils/embedding.py
+++ src/utils/embedding.py
@@ -2,16 +2,17 @@ from sklearn import metrics
 import numpy as np
 import networkx as nx
 from node2vec import Node2Vec
+from utils import weights
 
 
-def embed_data(data, dims=2, walk_length=100, num_walks=10, seed=0):
+def embed_data(data, weight_fun=weights.reciprocal, dims=2, walk_length=100, num_walks=10, seed=0):
     graph = build_graph(data)
     embeddings = embed_graph(
         graph, dims=dims, walk_length=walk_length, num_walks=num_walks, seed=seed)
     return embeddings
 
 
-def build_graph(data):
+def build_graph(data, weight_fun=weights.reciprocal):
     # compute distances between the points
     dists = metrics.pairwise_distances(data)
 
@@ -33,9 +34,7 @@ def build_graph(data):
         elif dist == 0:
             raise Exception("A pair of nodes with zero distance occurred")
 
-        weight = 1/dist
-
-        g.add_edge(u, v, weight=weight)
+        g.add_edge(u, v, weight=weight_fun(dist))
 
         if nx.is_connected(g):
             break
