0c2b17fe7cafd2f2ddc91ca1df5d61a119649e85
radajakub <dev.jakubrada@icloud.com>
2023-05-28 22:25:19 +0900

    improved visualization

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -6,46 +6,61 @@ import networkx as nx
 matplotlib.rcParams['figure.figsize'] = [20, 5]
 
 
-def show_data(data, graph, labels=None, aspect='auto', outpath='', show_numbers=False, dpi=300):
+def show_data(data, graph=None, labels=None, aspect='auto', square=False, outpath='', show_numbers=False, title='', dpi=300):
     if data.shape[1] > 3 and data.shape[1] < 2:
         raise Exception(
             "cannot visualize data with dimension higher than 3 or lower than 2")
 
-    # create two plots in one image
     if data.shape[1] == 2:
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-        # set aspect ratio
-        ax1.set_aspect(aspect, adjustable='box')
-
+        ax = plt.figure().add_subplot()
         # plot data
-        ax1.scatter(data[:, 0], data[:, 1], c=labels)
+        ax.scatter(data[:, 0], data[:, 1], c=labels)
+        # show numbers of datasamples (useful only for small datasets)
         if show_numbers:
             for i in range(data.shape[0]):
-                ax1.text(data[i, 0], data[i, 1], str(i))
-    else:
-        fig = plt.figure()
-        ax1 = plt.subplot(1, 2, 1, projection='3d')
-        ax2 = plt.subplot(1, 2, 2)
+                ax.text(data[i, 0], data[i, 1], str(i))
+        ax.set_aspect(aspect, adjustable='box')
+        if square:
+            ax.set_box_aspect(1)
 
-        # set aspect ratio
-        ax1.set_aspect(aspect, adjustable='box')
-
+        if graph is not None:
+            # plot existing edges in a graph into the points
+            for u, v in graph.edges():
+                points = data[[u, v], :]
+                ax.plot(points[:, 0], points[:, 1], color='k', alpha=0.5)
+    elif data.shape[1] == 3:
+        ax = plt.figure().add_subplot(projection='3d')
         # plot data
-        ax1.scatter(data[:, 0], data[:, 1], data[:, 2], c=labels)
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=labels)
+        # show numbers of datasamples (useful only for small datasets)
         if show_numbers:
             for i in range(data.shape[0]):
-                ax1.text(data[i, 0], data[i, 1], data[i, 2], str(i))
+                ax.text(data[i, 0], data[i, 1], data[i, 2], str(i))
+        ax.set_aspect(aspect, adjustable='box')
+        if square:
+            ax.set_box_aspect((1, 1, 1))
 
-    show_graph(graph, labels=labels, ax=ax2)
+        if graph is not None:
+            # plot existing edges in a graph into the points
+            for u, v in graph.edges():
+                points = data[[u, v], :]
+                ax.plot(points[:, 0], points[:, 1],
+                        points[:, 2], color='k', alpha=0.5)
+
+    if title != '':
+        ax.set_title(title)
+    ax.grid(visible=True, which='both', axis='both')
 
     # save plot if outpath is specified
     if outpath != '':
-        fig.savefig(outpath, dpi=dpi)
+        plt.savefig(outpath, facecolor='white', transparent=False,
+                    dpi=dpi, bbox_inches='tight')
 
 
-def show_graph(graph, labels=None, ax=None, outpath='', dpi=300):
-    if ax == None:
-        _, ax = plt.subplots(1, 1)
+def show_graph(graph, labels=None, outpath='', ax=None, title='', dpi=300):
+    if ax is None:
+        ax = plt.figure().add_subplot()
+    ax.set_title(title)
 
     layout = nx.spring_layout(graph)
     nx.draw(graph, pos=layout, ax=ax, with_labels=True,
@@ -56,22 +71,24 @@ def show_graph(graph, labels=None, ax=None, outpath='', dpi=300):
         graph, pos=layout, ax=ax, edge_labels=labels)
 
     if outpath != '':
-        ax.figure.savefig(outpath, dpi=dpi)
+        plt.savefig(outpath, facecolor='white', transparent=False,
+                    dpi=dpi, bbox_inches='tight')
 
 
-def show_embedding(embeddings, labels=None, aspect='auto', outpath='', show_numbers=False, title='', subtitle='', dpi=300):
+def show_embedding(embeddings, labels=None, aspect='auto', square=False, outpath='', show_numbers=False, title='', dpi=300):
     if embeddings.shape[1] != 2:
         raise Exception(
             "cannot visualize embeddings with dimension other than 2")
 
-    fig, ax = plt.subplots(1, 1)
+    ax = plt.figure().add_subplot()
     x = embeddings[:, 0]
     y = embeddings[:, 1]
 
     ax.set_aspect(aspect, adjustable='box')
-    ax.set_box_aspect(1)
+    if square:
+        ax.set_box_aspect(1)
     ax.set_title(title)
-    ax.grid(visible=True)
+    ax.grid(visible=True, which='both', axis='both')
     ax.scatter(x, y, c=labels)
 
     if show_numbers:
@@ -80,35 +97,5 @@ def show_embedding(embeddings, labels=None, aspect='auto', outpath='', show_numb
 
     # save plot if outpath is specified
     if outpath != '':
-        plt.savefig(outpath, facecolor='white', transparent=False, dpi=dpi)
-
-
-def show_graph_in_data(data, graph, labels=None, aspect='auto', outpath='', show_numbers=False, title='', dpi=300):
-    if data.shape[1] != 2:
-        raise Exception(
-            "cannot visualize data with dimension other than 2")
-
-    # create two plots in one image
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    fig.suptitle(title)
-
-    # set aspect ratio
-    ax1.set_aspect(aspect, adjustable='box')
-
-    # plot data
-    ax1.scatter(data[:, 0], data[:, 1], c=labels)
-    if show_numbers:
-        for i in range(data.shape[0]):
-            ax1.text(data[i, 0], data[i, 1], str(i))
-
-    # plot existing edges in a graph into the points
-    for u, v in graph.edges():
-        points = data[[u, v], :]
-        ax1.plot(points[:, 0], points[:, 1], color='k', alpha=0.5)
-
-    # plot graph
-    show_graph(graph, labels=labels, ax=ax2)
-
-    # save plot if outpath is specified
-    if outpath != '':
-        fig.savefig(outpath, dpi=dpi)
+        plt.savefig(outpath, facecolor='white', transparent=False,
+                    dpi=dpi, bbox_inches='tight')
